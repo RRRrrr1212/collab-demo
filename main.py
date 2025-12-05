412db7c8f0da201859296fa163ee0a4c813f8445
Dragon9471 <p0903369799@myyahoo.com>
2025-12-05 10:23:04 +0800

    add-op

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 print("hello world")
+
+print("=== Python Calculator ===")
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+sum = a + b
+print(f"Result: {a} + {b} = {sum}")
+
+print("System Check OK.")
